719984aba164508e6f953b4b90f097fcd1cd878a
Kobzar Liliia <olegkobzar2209@gmail.com>
2021-12-12 16:07:28 +0200

    code modified according to comments

--- src/tutorial-1/components/ClassProfile.js
+++ src/tutorial-1/components/ClassProfile.js
@@ -1,25 +1,11 @@
 import React from "react";
 
 class ClassProfile extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.date = this.props.registredAt.split('.');
-    }
-
-    monthToStr(month) {
-        return month > 12 || month < 1
-            ? null
-            : 'январь,февраль,март,апрель,май,июнь,июль,август,сентябрь,октябрь,ноябрь,декабрь'.split(',')[
-                month - 1
-            ];
-    }
-
     render() {
         return(
             <div className="profile">
-                <p>Привет, {this.props.name}!</p>
-                <p>Дата регистрации: {`${this.date[0]} ${this.monthToStr(Number(this.date[1]) - 1)} ${this.date[2]}`}</p>
+                <p>Привет, {this.props.name} {this.props.surname}!</p>
+                <p>Дата регистрации: {this.props.registredAt}</p>
                 <span>* я классовый компонент</span>
             </div>
         );
--- src/tutorial-1/components/FunctionProfile.js
+++ src/tutorial-1/components/FunctionProfile.js
@@ -1,21 +1,10 @@
 import React from "react";
 
-function FunctionProfile ({ name, registredAt }) {
-
-    const date = registredAt.split('.');
-
-    function monthToStr(month) {
-        return month > 12 || month < 1
-            ? null
-            : 'январь,февраль,март,апрель,май,июнь,июль,август,сентябрь,октябрь,ноябрь,декабрь'.split(',')[
-                month - 1
-            ];
-    }
-
+function FunctionProfile ({ name, surname, registredAt }) {
     return(
         <div className="profile">
-            <p>Привет, {name}!</p>
-            <p>Дата регистрации: {`${date[0]} ${monthToStr(Number(date[1]) - 1)} ${date[2]}`}</p>
+            <p>Привет, {name} {surname}!</p>
+            <p>Дата регистрации: {registredAt}</p>
             <span>* я функциональный компонент</span>
         </div>
     )
