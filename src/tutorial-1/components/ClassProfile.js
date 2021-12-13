d56476730b35997e36bd32a8a262dba9703559ca
Kobzar Liliia <olegkobzar2209@gmail.com>
2021-12-13 14:34:43 +0200

    fixed name output

--- src/tutorial-1/components/ClassProfile.js
+++ src/tutorial-1/components/ClassProfile.js
@@ -4,7 +4,7 @@ class ClassProfile extends React.Component {
     render() {
         return(
             <div className="profile">
-                <p>Привет, {this.props.name} {this.props.surname}!</p>
+                <p>Привет, {this.props.name.split(' ')[0]}!</p>
                 <p>Дата регистрации: {this.props.registredAt}</p>
                 <span>* я классовый компонент</span>
             </div>
--- src/tutorial-1/components/FunctionProfile.js
+++ src/tutorial-1/components/FunctionProfile.js
@@ -3,7 +3,7 @@ import React from "react";
 function FunctionProfile ({ name, surname, registredAt }) {
     return(
         <div className="profile">
-            <p>Привет, {name} {surname}!</p>
+            <p>Привет, {name.split(' ')[0]}!</p>
             <p>Дата регистрации: {registredAt}</p>
             <span>* я функциональный компонент</span>
         </div>
