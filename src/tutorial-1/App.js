fe6d29c3e8a53862ed30aa392947cb9a8c4be630
Kobzar Liliia <olegkobzar2209@gmail.com>
2021-12-13 14:26:02 +0200

    fixed name output

--- src/tutorial-1/App.js
+++ src/tutorial-1/App.js
@@ -4,8 +4,8 @@ import FunctionProfile from "./components/FunctionProfile";
 function App() {
     return(
         <div>
-            <ClassProfile name="Василий" surname="Давиденко" registredAt={(new Date(2021, 5, 22)).toLocaleDateString('ru-RU', { day:'numeric', month:'long', year:'numeric'})} />
-            <FunctionProfile name="Георгий" surname="Тищенко" registredAt={(new Date(2021, 5, 22)).toLocaleDateString('ru-RU', { day:'numeric', month:'long', year:'numeric'})} />
+            <ClassProfile name="Василий Давиденко" registredAt={(new Date(2021, 5, 22)).toLocaleDateString('ru-RU', { day:'numeric', month:'long', year:'numeric'})} />
+            <FunctionProfile name="Георгий Тищенко" registredAt={(new Date(2021, 5, 22)).toLocaleDateString('ru-RU', { day:'numeric', month:'long', year:'numeric'})} />
         </div>
     );
 }
